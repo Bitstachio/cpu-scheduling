e69613bd349bdafc7a1116dffc1865119eb9f590
BarbodH <habibi.k.barbod@gmail.com>
2025-02-19 15:24:16 -0500

    Add turn around & wait time calculation for FCFS, SJF, Priority

--- schedule_priority.c
+++ schedule_priority.c
@@ -22,7 +22,24 @@ void add(char *name, int priority, int burst) {
 
 void schedule() {
     Node *node;
+    int numProcesses = 0;
+
+    double turnAroundTime = 0;
+    double turnAroundTimeTotal = 0;
+    double waitTimeTotal = 0;
+
     while ((node = pop(&head)) != NULL) {
         run(node->task, node->task->burst);
+
+        waitTimeTotal += turnAroundTime;
+        turnAroundTime += node->task->burst;
+        turnAroundTimeTotal += turnAroundTime;
+
+        numProcesses++;
     }
+
+    printf("\nAverage waiting time = %.2f", waitTimeTotal / numProcesses);
+    printf("\nAverage turnaround time = %.2f", turnAroundTimeTotal / numProcesses);
+    // TODO: Average response time
+    // printf("Average response time = %.2f", );
 }
