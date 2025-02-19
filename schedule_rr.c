5cf24eb7b0e577b58ffb98ee45fe6af699154c7b
BarbodH <habibi.k.barbod@gmail.com>
2025-02-19 18:37:22 -0500

    Add turn around & wait time calculation for RR

--- schedule_rr.c
+++ schedule_rr.c
@@ -6,29 +6,61 @@
 #include <string.h>
 
 Queue queue = {.head = NULL, .tail = NULL};
+int id = 0;
+int numProcesses = 0;
 
 void add(char *name, int priority, int burst) {
     Task *task = malloc(sizeof(Task));
     task->name = strdup(name);
     task->priority = priority;
     task->burst = burst;
+    task->tid = id++;
 
     insertQueue(&queue, task);
+    numProcesses++;
 }
 
 void schedule() {
     Node *node;
+    double completionTimes[numProcesses];
+
+    int burstTimes[numProcesses];
+    for (int i = 0; i < numProcesses; i++) {
+        burstTimes[i] = -1;
+    }
+
+    double turnAroundTime = 0;
+    double turnAroundTimeTotal = 0;
+    double waitTimeTotal = 0;
+
     while ((node = removeQueue(&queue)) != NULL) {
         Task *task = node->task;
+
+        if (burstTimes[task->tid] < 0) {
+            burstTimes[task->tid] = task->burst;
+        }
 
         const int slice = node->next == NULL       ? task->burst
                           : task->burst >= QUANTUM ? QUANTUM
                                                    : task->burst;
         run(node->task, slice);
 
+        turnAroundTime += slice;
+        completionTimes[task->tid] = turnAroundTime;
+
         task->burst -= slice;
         if (task->burst > 0) {
             insertQueue(&queue, task);
         }
     }
+
+    for (int i = 0; i < numProcesses; i++) {
+        turnAroundTimeTotal += completionTimes[i];
+        waitTimeTotal += completionTimes[i] - burstTimes[i];
+    }
+
+    printf("\nAverage waiting time = %.2f", waitTimeTotal / numProcesses);
+    printf("\nAverage turnaround time = %.2f", turnAroundTimeTotal / numProcesses);
+    // TODO: Average response time
+    // printf("Average response time = %.2f", );
 }
