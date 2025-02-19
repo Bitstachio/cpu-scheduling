477a8047c3b076c4b561083818af2f5f7fb89ede
BarbodH <habibi.k.barbod@gmail.com>
2025-02-19 11:31:18 -0500

    Implement basic RR

--- schedule_rr.c
+++ schedule_rr.c
@@ -1,3 +1,32 @@
-void add(char *name, int priority, int burst) {}
+#include "list.h"
+#include "schedulers.h"
 
-void schedule() {}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Queue queue = {.head = NULL, .tail = NULL};
+
+void add(char *name, int priority, int burst) {
+    Task *task = malloc(sizeof(Task));
+    task->name = strdup(name);
+    task->priority = priority;
+    task->burst = burst;
+
+    insertQueue(&queue, task);
+}
+
+void schedule() {
+    Node *node;
+    while ((node = removeQueue(&queue)) != NULL) {
+        Task *task = node->task;
+
+        const int slice = task->burst >= QUANTUM ? QUANTUM : task->burst;
+        run(node->task, slice);
+
+        task->burst -= slice;
+        if (task->burst > 0) {
+            insertQueue(&queue, task);
+        }
+    }
+}
