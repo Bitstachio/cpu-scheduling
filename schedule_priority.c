ce4102a641d4cfbb04d564debadd69bd1573e49b
BarbodH <habibi.k.barbod@gmail.com>
2025-02-18 23:23:15 -0500

    Implement basic Priority

--- schedule_priority.c
+++ schedule_priority.c
@@ -1,3 +1,28 @@
-void add(char *name, int priority, int burst) {}
+#include "list.h"
+#include "schedulers.h"
 
-void schedule() {}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Node *head = NULL;
+
+int compareByPriority(const Task *a, const Task *b) {
+    return b->priority - a->priority;
+}
+
+void add(char *name, int priority, int burst) {
+    Task *task = malloc(sizeof(Task));
+    task->name = strdup(name);
+    task->priority = priority;
+    task->burst = burst;
+
+    insertSorted(&head, task, compareByPriority);
+}
+
+void schedule() {
+    Node *node;
+    while ((node = pop(&head)) != NULL) {
+        run(node->task, node->task->burst);
+    }
+}
